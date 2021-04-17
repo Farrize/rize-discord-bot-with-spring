01e3e0ebd1e5bcf826701234acb4535eb53922f9
Muhammad Farras <strikeit3214@gmail.com>
2021-04-17 10:03:24 +0700

    Inisialisasi Pertama

---
+++ src/main/java/com/rizebot/rizebot/RizeBotApplication.java
@@ -0,0 +1,13 @@
+package com.rizebot.rizebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizeBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizebot/rizebot/RizeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizebot.rizebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
